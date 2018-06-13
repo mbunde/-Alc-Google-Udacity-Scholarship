9b9d0b33de35c4771f16769367c078a165c9af53
Collins <collinsbunde@hotmail.com>
2018-06-14 00:27:19 +0300

    Add files via upload

---
+++ Murder Mystery (3-4).js
@@ -0,0 +1,37 @@
+/*
+ * Programming Quiz: Murder Mystery (3-4)
+ */
+
+// change the value of `room` and `suspect` to test your code
+var room = "dining room";
+var suspect = "Mr. Parkes";
+
+var weapon = "";
+var solved = false;
+
+if (/* your conditional goes here */
+room ==="gallery") {
+    weapon ="trophy";
+    if(suspect==="Ms. Van Cleve"){
+        solved=true;
+    }
+
+} else if (/* your conditional goes here */room==="ballroom") {
+     weapon ="poison";
+    if(suspect==="Mr.Kalehoff"){
+        solved=true;
+    }
+} else if (/* your conditional goes here */ room==="dining room") {
+  weapon ="knife";
+    if(suspect==="Mr. Parkes"){
+        solved=true;
+}
+}else {
+    weapon ="pool stick";
+    if(suspect==="Mrs. Sparr"){
+        solved=true;
+    }
+}
+
+if (solved) {console.log(suspect +" did it in the "+ room + " with the "+ weapon +"!");
+}
