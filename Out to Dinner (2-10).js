597966300de2880e4a2e0a42646793e9ef76ce0a
Collins <collinsbunde@hotmail.com>
2018-06-14 00:29:30 +0300

    Add files via upload

---
+++ Out to Dinner (2-10).js
@@ -0,0 +1,8 @@
+/*
+ * Programming Quiz: Out to Dinner (2-10)
+ */
+// your code goes here
+var bill = 10.25 + 3.99 + 7.15;
+var tip = 0.15 * bill;
+var total = tip + bill;
+console.log("$"+ total.toFixed(2));
---
+++ Semicolons! (2-8).js
@@ -0,0 +1,8 @@
+/*
+ * Programming Quiz: Semicolons! (2-8)
+ */
+
+// your code goes here
+var thingOne="Collins";
+var thingTwo="Bunde";
+console.log(thingOne + thingTwo);
